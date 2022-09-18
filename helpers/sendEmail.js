e9e49f0c1c389201cb7700532fc4670a1b386414
VorobyovaAnna <anyasonn1k@gmail.com>
2022-09-18 15:42:47 +0300

    hid the email

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,13 +1,13 @@
 const sgMail = require("@sendgrid/mail");
 require("dotenv").config();
 
-const {SENDGRID_API_KEY} = process.env;
+const {SENDGRID_API_KEY, EMAIL} = process.env;
 
 sgMail.setApiKey(SENDGRID_API_KEY);
 
 const sendEmail = async (data) => {
     try {
-        const email = { ...data, from: "anyasonn1k@gmail.com" };
+        const email = { ...data, from: EMAIL };
         await sgMail.send(email);
         return true;
     } catch (error) {
